bc6f89e039c736aebf1d54b91cf5454d83bcca86
Gigi <dergigi@pm.me>
2025-09-28 08:54:20 +0200

    chore: adjust utils export

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -23,7 +23,7 @@ export function shortenString(
 
 // Re-export URL utilities from centralized module
 export { extractDomainFromUrl } from './utils/urlUtils';
-export { extractNip19Identifiers, NIP19_BOUNDARY_REGEX, type Nip19Prefix } from './utils/nostrIdentifiers';
+export { extractNip19Identifiers, decodeNip19Pointer, NIP19_BOUNDARY_REGEX, type Nip19Prefix, type Nip19Pointer } from './utils/nostrIdentifiers';
 
 /**
  * Shortens an npub string using the standard format
