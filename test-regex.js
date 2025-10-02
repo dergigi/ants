a73a63c475ea9e7621fa7ac34d8811e2b7141b2a
Gigi <dergigi@pm.me>
2025-10-02 22:44:14 +0200

    chore(logs): remove console.log debug output from test-regex utility

--- test-regex.js
+++ test-regex.js
@@ -2,13 +2,9 @@
 const BY_TOKEN_REGEX = /(^|\s)by:([^\s),.;]+)(?=[\s),.;]|$)/gi;
 
 const query = "by:_@dergigi.com";
-console.log('Testing regex with query:', query);
 
 BY_TOKEN_REGEX.lastIndex = 0;
 let match;
 while ((match = BY_TOKEN_REGEX.exec(query)) !== null) {
-  console.log('Match found:', match);
-  console.log('Full match:', match[0]);
-  console.log('Prefix:', match[1]);
-  console.log('Token:', match[2]);
+  // debug output removed
 }
