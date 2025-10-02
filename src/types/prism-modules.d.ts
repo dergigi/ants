902fe725d67db2828af473c4654a0a6ff52967e6
Gigi <dergigi@pm.me>
2025-10-02 21:06:52 +0200

    chore(types): add shims for prismjs language components to satisfy tsc

---
+++ src/types/prism-modules.d.ts
@@ -0,0 +1,9 @@
+// Minimal module shims for Prism language components used by our dynamic loader
+// These modules only need to be importable for side effects; no typings required.
+declare module 'prismjs/components/prism-bash';
+declare module 'prismjs/components/prism-typescript';
+declare module 'prismjs/components/prism-javascript';
+declare module 'prismjs/components/prism-json';
+declare module 'prismjs/components/prism-css';
+declare module 'prismjs/components/prism-markdown';
+
